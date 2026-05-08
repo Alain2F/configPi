dbf670cffc7f93c89520866dbfe2ef97f399966a
Alain F <toquark59@hotmail.com>
2026-05-08 14:27:04 -0400

    Update library installation script for Arduino
    Removed outdated library installations for ESPAsyncTCP, AsyncTCP, and FSBrowser. Added installation for arduinoWebSockets and WEMOS SHT3x Arduino Library.

--- ArduinoLibViaZip.sh
+++ ArduinoLibViaZip.sh
@@ -23,19 +23,6 @@
 #
 
 
-# Pour ESPAynchTCP
-# https://github.com/me-no-dev/ESPAsyncTCP/archive/refs/heads/master.zip
-cd ~/Downloads
-wget https://github.com/me-no-dev/ESPAsyncTCP/archive/refs/heads/master.zip 
-unzip master.zip -d ~/Arduino/libraries
-mv ~/Arduino/libraries/ESPAsyncTCP-master ~/Arduino/libraries/ESPAsyncTCP
-rm master.zip 
-# pour esp32 
-cd ~/Downloads
-wget https://github.com/me-no-dev/AsyncTCP/archive/refs/heads/master.zip
-unzip master.zip -d ~/Arduino/libraries
-mv ~/Arduino/libraries/AsyncTCP-master ~/Arduino/libraries/AsyncTCP
-rm master.zip 
 # ajouter MyHeaders 
   echo "Création du répertoire MyHeaders"
 cd ~/configPi/
@@ -50,30 +37,16 @@ wget https://github.com/tzapu/WiFiManager/archive/refs/heads/master.zip
 unzip master.zip -d ~/Arduino/libraries
 mv ~/Arduino/libraries/WiFiManager-master ~/Arduino/libraries/WiFiManager
 rm master.zip
-# FSBrowser pour ESP32  https://github.com/cotestatnt/esp-fs-webserver/archive/refs/tags/1.3.0.zip  
-cd ~/Downloads
-#deniere version 
-# wget https://github.com/cotestatnt/esp-fs-webserver/archive/refs/heads/master.zip
-# unzip master.zip -d ~/Arduino/libraries
-# mv ~/Arduino/libraries/esp-fs-webserver-master ~/Arduino/libraries/esp-fs-webserver
-# rm master.zip
-# pour la version 1.2.8 
-wget https://github.com/cotestatnt/esp-fs-webserver/archive/refs/tags/1.2.8.zip
-unzip 1.2.8.zip -d ~/Arduino/libraries
-# esp-fs-webserver-1.2.8
-mv ~/Arduino/libraries/esp-fs-webserver-1.2.8 ~/Arduino/libraries/esp-fs-webserver
-rm 1.2.8.zip
+
 # Avec Arduino WebSocket
+cd ~/Downloads
 wget https://github.com/Links2004/arduinoWebSockets/archive/refs/heads/master.zip
 unzip master.zip -d ~/Arduino/libraries
 mv ~/Arduino/libraries/arduinoWebSockets-master ~/Arduino/libraries/arduinoWebSockets
 rm master.zip
-# Pour la version FSBrowser Aynch TCP pour ESP32
-wget https://github.com/cotestatnt/async-esp-fs-webserver/archive/refs/heads/master.zip
-unzip master.zip -d ~/Arduino/libraries
-mv ~/Arduino/libraries/async-esp-fs-webserver-master ~/Arduino/libraries/async-esp-fs-webserver
-rm master.zip
+
 # Pour SHT30 sur Wemos
+cd ~/Downloads
 wget https://github.com/wemos/WEMOS_SHT3x_Arduino_Library/archive/master.zip
 unzip master.zip -d ~/Arduino/libraries
 mv ~/Arduino/libraries/WEMOS_SHT3x_Arduino_Library-master ~/Arduino/libraries/WEMOS_SHT3x_Arduino_Library
